4646502e2b5177e35f18b4ab22f5447cb509134e
Angela Carvalho <angelalopo@outlook.com>
2024-06-24 15:51:52 -0300

    atualiza rotas

--- src/ApplicationRoutes.jsx
+++ src/ApplicationRoutes.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Sobre from './pages/Sobre/Sobre'
+import Denuncia from './pages/Denuncia/Denuncia'
+import Contato from './pages/Contato/Contato'
+import Artigos from './pages/Artigos/Artigos'
 import Footer from './components/Footer/Footer'
 import Menu from './components/Menu/Menu'
 
@@ -10,6 +14,10 @@ function ApplicationRoutes() {
         <Menu />
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/denuncia" element={<Denuncia />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/artigos" element={<Artigos />} />
         </Routes>
         <Footer />
         </BrowserRouter>
